a959f7ec8e2f5cc3c98da7126b798386e593ed9d
ahalitogullari <ahalitogullari17@ku.edu.tr>
2022-06-08 16:21:35 +0300

    main class created and makefile implemented

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+int main(int argc, char *argv[])
+{
+	printf("\nComp 305 project\n");
+	return 0;
+}
