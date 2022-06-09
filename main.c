eaecc6aae70caecf2a792dc06f234f7fdd4c866b
ahalitogullari <ahalitogullari17@ku.edu.tr>
2022-06-09 20:52:16 +0300

    mergeSort function implemented

--- main.c
+++ main.c
@@ -5,9 +5,22 @@
 #include <stdbool.h>
 
 
-int findLongestNumber(int numbers[], int totalPerson)
-{
+int mergeSort(int **numbers, int **personList,  int totalPerson)
+{	
+	int i=0, j=0, temp=0, temp2=0;
+	for(i=0; i<totalPerson*6; i++) {
+		for(j=0; j<totalPerson*6; j++) {
+			if(numbers[i] <  numbers[j]) {
+				temp = numbers[j];
+				numbers[j] = numbers[i];
+				numbers[i] = temp;
 
+				temp2 = personList[j];
+				personList[j] = personList[i];
+				personList[i] = temp2;
+			}
+		}	
+	}
 	return 0;
 }
 
@@ -32,18 +45,24 @@ int main(int argc, char *argv[])
 	for(i=0; i<2; i++) {
 		fgets(str, maxChar, readFile);
 		totalPerson = atoi(str);
-		int numbers[totalPerson*6];
+		int **numbers[totalPerson*6];
+		int **personList[totalPerson*6];
 		int countNumbers = 0; //should be 6 * totalPerson	
 		for(j=0; j<totalPerson; j++){
 			fgets(str, maxChar, readFile);
 			char *splittedNumber = strtok(str, " ");
 			while(splittedNumber != NULL) { //split string to find number
 				numbers[countNumbers] = atoi(splittedNumber);
+				personList[countNumbers] = j;
 				splittedNumber = strtok(NULL, " ");
 				countNumbers++;
 			}
 		}
-		findLongestNumber(numbers, totalPerson);
+		mergeSort(numbers, personList,  totalPerson);
+		int z=0;
+		for(z=0; z<totalPerson*6; z++) {
+			printf("%d %d \n", numbers[z], personList[z]);
+		}
 	}
 	fclose(readFile);
 	return 0;
